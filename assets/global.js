a2b8876140448a0a174932126a864824d469b8a9
Abhijit-webtwine <abhijit.webtwine@gmail.com>
2026-01-23 17:59:51 +0530

    Related Product

--- assets/global.js
+++ assets/global.js
@@ -3417,33 +3417,55 @@ class AccordionTab extends HTMLElement {
 customElements.define('accordion-tab', AccordionTab);
 
 class ProductRecommendations extends HTMLElement {
+  observer = undefined;
+
   constructor() {
     super();
   }
 
   connectedCallback() {
-    new IntersectionObserver(this.handleIntersection.bind(this), {rootMargin: '0px 0px 600px 0px'}).observe(this);
+    this.initializeRecommendations(this.dataset.productId);
   }
 
-  handleIntersection(entries, observer) {
-    if (!entries[0].isIntersecting) return;
-    observer.unobserve(this);
+  initializeRecommendations(productId) {
+    this.observer?.unobserve(this);
+    this.observer = new IntersectionObserver(
+      (entries, observer) => {
+        if (!entries[0].isIntersecting) return;
+        observer.unobserve(this);
+        this.loadRecommendations(productId);
+      },
+      { rootMargin: '0px 0px 400px 0px' }
+    );
+    this.observer.observe(this);
+  }
 
-    fetch(this.dataset.url)
-      .then(response => response.text())
-      .then(text => {
+  loadRecommendations(productId) {
+    fetch(`${this.dataset.url}&product_id=${productId}&section_id=${this.dataset.sectionId}`)
+      .then((response) => response.text())
+      .then((text) => {
         const html = document.createElement('div');
         html.innerHTML = text;
         const recommendations = html.querySelector('product-recommendations');
-        if (recommendations && recommendations.innerHTML.trim().length) {
+
+        if (recommendations?.innerHTML.trim().length) {
           this.innerHTML = recommendations.innerHTML;
         }
+
+        if (!this.querySelector('slideshow-component') && this.classList.contains('complementary-products')) {
+          this.remove();
+        }
+
+        if (html.querySelector('.grid__item')) {
+          this.classList.add('product-recommendations--loaded');
+        }
       })
-      .catch(e => {
+      .catch((e) => {
         console.error(e);
       });
   }
 }
+
 customElements.define('product-recommendations', ProductRecommendations);
 
 class HeaderMenuToggle extends HTMLElement {
